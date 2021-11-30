e31b9e4040636a1014696c9b1479d7c49067c90f
Boopathi <boopathi@valorpaytech.com>
2021-11-30 18:20:44 +0530

    newly added

--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Homepage from '../app/pages/Homepage/Homepage';
-import Jersey from '../app/pages/MainApp/pages/Jersey/Product_Jersey';
-import Shirt from '../app/pages/MainApp/pages/shirt/Product_Shirt';
-import Layout from '../Layout';
+import Layout from './pages/Ecommerce';
+import Homepage from './pages/Ecommerce/pages/Homepage';
+import Jersey from './pages/Ecommerce/pages/Jersey';
+import Shirt from './pages/Ecommerce/pages/Shirt';
 import Error404 from './pages/Error404';
-import SignUp from './pages/SignUp/SignUp';
+import SignUp from './pages/SignUp';
 
 export default function PrivateRouteNew() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SignUp />} />
-        <Route path='*' element={<Error404 />} />
         <Route path='eCommerce' element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path=':eCommerceJersey' element={<Jersey />} />
           <Route path='shirt' element={<Shirt />} />
         </Route>
+        <Route path='*' element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
