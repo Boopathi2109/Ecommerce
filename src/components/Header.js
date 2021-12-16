215f59b94649dfd69204dfe4fa94baa4df1d88b6
Boopathi <boopathi@valorpaytech.com>
2021-12-16 15:48:48 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import logo from '../assets/logo_1.png';
 
 function Header() {
   return (
-    <div className=' md:grid grid-cols-3 bg-green-300  h-16  flex items-center '>
+    <div className=' md:grid md:grid-cols-3 grid grid-cols-3 bg-green-300  h-16  flex items-center '>
       <div className='flex justify-start '>
         <img width={40} src={logo} />
       </div>
       <div className='flex justify-center '>
-        <H3>CR7 cristiano Ronaldo</H3>
+        <H3>Store</H3>
       </div>
       <div className='flex justify-end '>
         <img width={40} height={40} src={logo} />
