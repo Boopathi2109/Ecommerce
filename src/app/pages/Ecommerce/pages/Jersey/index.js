c20fd0dd595591e51062b8e3ccf86f0bd19344a6
Boopathi <boopathi@valorpaytech.com>
2021-12-17 19:15:53 +0530

    new

--- src/app/pages/Ecommerce/pages/Jersey/index.js
+++ src/app/pages/Ecommerce/pages/Jersey/index.js
@@ -1,5 +1,6 @@
 import Button from '@material-tailwind/react/Button';
 import Card from '@material-tailwind/react/Card';
+import H5 from '@material-tailwind/react/Heading5';
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Data from '../../../../Json/Jersey.json';
@@ -18,11 +19,15 @@ function Jersey(params) {
       <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
         {Data.map((ele, i) => {
           return (
-            <Card className='mb-5 py-7 shadow-lg'>
+            <Card className=' py-7 shadow-lg'>
               <div key={i}>
                 <p>
+                  {ele.Date >= '16/12/2021' && <span className='absolute animate-bounce font-medium bg-transparent bg-green-300'>Top Left</span>}
                   <img src={`../images/${ele.image}`} />
                 </p>
+                <div className='flex justify-center mt-3'>
+                  <H5>New</H5>
+                </div>
                 <div className='flex justify-center mt-3'>
                   <Button color='teal' buttonType='filled' size='regular' rounded={true} block={false} iconOnly={false} ripple='light' onClick={() => handleNavigate()}>
                     Buy Now
